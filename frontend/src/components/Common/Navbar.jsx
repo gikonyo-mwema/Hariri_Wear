0233d2263f63427e6ec8a2cc86347205145088f4
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-02-27 14:53:55 +0000

    add CartDrawer component to Navbar with toggle functionality

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -7,8 +7,20 @@ import {
 
  } from "react-icons/hi2";
 import SearchBar from './SearchBar';
+import CartDrawer from '../Layout/CartDrawer';
+import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    
+    const toggleCartDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+
+         
+ 
+    };
+
     return (
         <>
             <nav className="container mx-auto flex items-center justify-between  py-4 px-6">
@@ -51,7 +63,9 @@ const Navbar = () => {
                 <Link to="/profile" className='hover:text-black'>
                     <HiOutlineUser className="h-6 w-6 text-gray-700" />
                 </Link>
-                <button className="relative hover:text-black">
+                <button
+                    onClick={toggleCartDrawer}
+                    className="relative hover:text-black">
                     <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
                     <span className="absolute -top-1 bg-[var(--color-primary)] text-white text-xs rounded-full px-2 py-0.5">4</span>
 
@@ -69,6 +83,7 @@ const Navbar = () => {
                 </button>
              </div>
             </nav>
+            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
         </>
     );
 }
